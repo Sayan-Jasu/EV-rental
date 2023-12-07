359bfc646b65adf4f4c7821529c7ec7633293b39
Sayan Jasu <76862569+Sayan-Jasu@users.noreply.github.com>
2023-12-07 21:02:43 +0530

    Update script.js 3

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=36f9be70cd0cd94edcf01375b6650ab6&page=1';
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=36f9be70cd0cd94edcf01375b6650ab6&query=";
-
 const b1 = document.querySelector("#home");
 const b2 = document.querySelector("#about");
 const b3 = document.querySelector("#rent");
